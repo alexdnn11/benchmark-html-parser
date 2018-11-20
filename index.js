ff1b0d48be2709f532954f47fbc1f47dcd739fb1
alexdnn11 <aliaksandr.dubinin@altoros.com>
2018-11-20 13:51:39 +0300

    Add loop each html files

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// include modules
 const fs = require('fs');
+const path = require('path');
 const readline = require('readline');
 const {google} = require('googleapis');
 const cheerio = require('cheerio');
+var klawSync = require('klaw-sync');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
@@ -10,40 +13,94 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 // time.
 const TOKEN_PATH = 'token.json';
 
-// Load client secrets from a local file.
-// fs.readFile('credentials.json', (err, content) => {
-//     if (err) return console.log('Error loading client secret file:', err);
-//     // Authorize a client with credentials, then call the Google Sheets API.
-//
-//     authorize(JSON.parse(content), listMajors);
-// });
-const file = '../benchmark-reports/reports-beautiful-finished/reports-beautiful/10tx-100-total-5-orgs/20181116T093853/reports/report20181116T094055-config-private.json.html';
-const roundsIds = ['round 1', "round 2", "round 3", "round 4", "round 5"];
-let row = [];
+//Other definition
+const spreadsheetId = '1Sa6cGuxxMSzVp397CbpzmNaY0pMwYGfQmkaSqIOBqkw';
+const valueInputOption = null;
+var   rows = []; //all parsed results
 
-fs.readFile(file, function (error, pgResp) {
-    if (error) {
-        console.log(error);
-    } else {
-        let bufferOriginal = Buffer.from(JSON.parse(JSON.stringify(pgResp)).data);
-        let content = bufferOriginal.toString('utf8');
-        const $ = cheerio.load(content);
-        roundsIds.forEach((id) => {
-            row = [];
-            let roundElemsTd = $("[id='"+id+"'] "+"table tr td").each( function(){
-                let value = $(this).text();
-                if (!value.includes("Process") && !value.includes("node local-client.js")){
-                    value = value.replace(' tps','');
-                    value = value.replace(' s','');
-                    value = value.replace('MB','');
-                    row.push(value);
-                }
-            });
-            console.log(row);
+var parse = new Promise(function(resolve, reject) {
+    // console.log('Doing');
+    //Collect all html-files in array paths[]
+    var pathList = getListHtmlFiles();
+
+    //loop for each html-file
+    pathList.forEach((filePath)=>{
+        fs.readFile(filePath.path, function (error, pgResp) {
+            if (error) {
+                console.log(error);
+            } else {
+                //get one full-row from html-file
+                parseValueFromHtml(pgResp);
+            }
         });
-    }
+    });
+    resolve(pathList);
 });
 
+parse.then(function successHandler(result) {
+    console.log(result.length);
+    // Load client secrets from a local file.
+    fs.readFile('credentials.json', (err, content) => {
+        // if (err) return console.log('Error loading client secret file:', err);
+        // var rowCurrent = 1;
+        // rows.push(row);
+        //
+        // let rowStr = '';
+        // row.forEach((cell)=>{
+        //      rowStr += ' ' + cell;
+        // });
+        // console.log(rowStr);
+
+        // Authorize a client with credentials, then call the Google Sheets API.
+        // var dataRange  ='Parsed!A'+rowCurrent+':N';
+        // var dataValues =row;
+        // authorize(JSON.parse(content), listMajors);
+        // rowCurrent +=1;
+    });
+}, function failureHandler(error) {
+    console.log('Wrong!');
+});
+
+function parseValueFromHtml(pgResp) {
+    const roundsIds = ['round 1', "round 2", "round 3", "round 4", "round 5"];
+    let bufferOriginal = Buffer.from(JSON.parse(JSON.stringify(pgResp)).data);
+    let content = bufferOriginal.toString('utf8');
+    const $ = cheerio.load(content);
+
+    roundsIds.forEach((id) => {
+        let row = []; //one full row after parsing
+        let roundElemsTd = $("[id='"+id+"'] "+"table tr td").each( function(){
+            let value = $(this).text();
+            if (!value.includes("Process") && !value.includes("node local-client.js")){
+                //clean measure
+                value = value.replace(' tps','');
+                value = value.replace(' s','');
+                value = value.replace('MB','');
+                row.push(value);
+            }
+
+        });
+        rows.push(row);
+    });
+}
+
+function getListHtmlFiles(){
+    let paths = [];
+    const filterFn = item => {
+        const basename = path.basename(item.path);
+        return basename.substr(basename.length - 4) !== 'json' &&  basename.substr(basename.length - 3) !== 'log'
+    };
+
+// The directory that you want to explore
+    var directoryToExplore = "../benchmark-reports/";
+
+    try {
+        paths = klawSync(directoryToExplore,{nodir: true, filter: filterFn});
+    } catch (err) {
+        console.error(err);
+    }
+    return paths;
+}
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
  * given callback function.
@@ -112,6 +169,49 @@ function listMajors(auth) {
             // Print columns A and E, which correspond to indices 0 and 4.
             rows.map((row) => {
                 console.log(`${row[0]}, ${row[5]}`);
+            });
+        } else {
+            console.log('No data found.');
+        }
+    });
+}
+
+function rowWriteToSpreadsheet(auth, dataRange, dataValues) {
+    const sheets = google.sheets({version: 'v4', auth});
+
+    const data = [{
+        dataRange,
+        dataValues,
+    }];
+    // Additional ranges to update ...
+    const resource = {
+        data,
+        valueInputOption,
+    };
+    // console.log(this);
+    this.sheetsService.spreadsheets.values.batchUpdate({
+        spreadsheetId,
+        resource,
+    }, (err, result) => {
+        if (err) {
+            // Handle error
+            console.log(err);
+        } else {
+            console.log('%d cells updated.', result.totalUpdatedCells);
+        }
+    });
+
+    sheets.spreadsheets.values.get({
+        spreadsheetId: '1Sa6cGuxxMSzVp397CbpzmNaY0pMwYGfQmkaSqIOBqkw',
+        range: 'Parsed!A9:Q',
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const rowsRead = res.data.values;
+        if (rowsRead.length) {
+            console.log('Name,	Max Latency:');
+            // Print columns A and E, which correspond to indices 0 and 4.
+            rowsRead.map((rowRead) => {
+                console.log(`${rowRead[0]}, ${rowRead[5]}`);
             });
         } else {
             console.log('No data found.');
